af3ad34772517d12d2cae30a5927716847045174
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-22 21:24:02 -0400

    authenticate in db.js checks password to log in

--- server/db.js
+++ server/db.js
@@ -1,9 +1,11 @@
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_auth_store_db');
-const uuid = require('uuid');
-const bcrypt = require('bcrypt');
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_auth_store_db"
+);
+const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 
-const createTables = async()=> {
+const createTables = async () => {
   const SQL = `
     DROP TABLE IF EXISTS favorites;
     DROP TABLE IF EXISTS users;
@@ -27,15 +29,19 @@ const createTables = async()=> {
   await client.query(SQL);
 };
 
-const createUser = async({ username, password})=> {
+const createUser = async ({ username, password }) => {
   const SQL = `
     INSERT INTO users(id, username, password) VALUES($1, $2, $3) RETURNING *
   `;
-  const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)]);
+  const response = await client.query(SQL, [
+    uuid.v4(),
+    username,
+    await bcrypt.hash(password, 5),
+  ]);
   return response.rows[0];
 };
 
-const createProduct = async({ name })=> {
+const createProduct = async ({ name }) => {
   const SQL = `
     INSERT INTO products(id, name) VALUES($1, $2) RETURNING *
   `;
@@ -43,7 +49,7 @@ const createProduct = async({ name })=> {
   return response.rows[0];
 };
 
-const createFavorite = async({ user_id, product_id })=> {
+const createFavorite = async ({ user_id, product_id }) => {
   const SQL = `
     INSERT INTO favorites(id, user_id, product_id) VALUES($1, $2, $3) RETURNING *
   `;
@@ -51,40 +57,50 @@ const createFavorite = async({ user_id, product_id })=> {
   return response.rows[0];
 };
 
-const destroyFavorite = async({ user_id, id })=> {
+const destroyFavorite = async ({ user_id, id }) => {
   const SQL = `
     DELETE FROM favorites WHERE user_id=$1 AND id=$2
   `;
   await client.query(SQL, [user_id, id]);
 };
 
-const authenticate = async({ username, password })=> {
+const authenticate = async ({ username, password }) => {
+  const sql = `SELECT password FROM users WHERE username=$1;`;
+  const result = await client.query(sql, [username]);
+  const hashedPass = result.rows[0].password;
+  const checkPass = await bcrypt.compare(password, hashedPass);
+  console.log("checkedPass: ", checkPass);
+  if (!checkPass) {
+    const error = new Error("not authorized");
+    error.status = 401;
+    throw error;
+  }
   const SQL = `
-    SELECT id, username FROM users WHERE username=$1;
+    SELECT id, username FROM users WHERE username=$1 AND password=$2;
   `;
-  const response = await client.query(SQL, [username]);
-  if(!response.rows.length){
-    const error = Error('not authorized');
+  const response = await client.query(SQL, [username, hashedPass]);
+  if (!response.rows.length) {
+    const error = Error("not authorized");
     error.status = 401;
     throw error;
   }
   return { token: response.rows[0].id };
 };
 
-const findUserWithToken = async(id)=> {
+const findUserWithToken = async (id) => {
   const SQL = `
     SELECT id, username FROM users WHERE id=$1;
   `;
   const response = await client.query(SQL, [id]);
-  if(!response.rows.length){
-    const error = Error('not authorized');
+  if (!response.rows.length) {
+    const error = Error("not authorized");
     error.status = 401;
     throw error;
   }
   return response.rows[0];
 };
 
-const fetchUsers = async()=> {
+const fetchUsers = async () => {
   const SQL = `
     SELECT id, username FROM users;
   `;
@@ -92,7 +108,7 @@ const fetchUsers = async()=> {
   return response.rows;
 };
 
-const fetchProducts = async()=> {
+const fetchProducts = async () => {
   const SQL = `
     SELECT * FROM products;
   `;
@@ -100,7 +116,7 @@ const fetchProducts = async()=> {
   return response.rows;
 };
 
-const fetchFavorites = async(user_id)=> {
+const fetchFavorites = async (user_id) => {
   const SQL = `
     SELECT * FROM favorites where user_id = $1
   `;
@@ -119,5 +135,5 @@ module.exports = {
   createFavorite,
   destroyFavorite,
   authenticate,
-  findUserWithToken
+  findUserWithToken,
 };
